af7d4757fb9dc97e4d4950765eecfcd46a44d5de
Victor Gil <victorgil1408@gmail.com>
2024-02-09 07:56:31 -0500

    Correct description of every exercise and labeled them

--- Exercises/LeetCodeE.py
+++ Exercises/LeetCodeE.py
@@ -14,6 +14,13 @@ class LeetCodeE:
         '''
         Constructor
         '''
+        
+    #You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+    #Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+    #The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+    
     @staticmethod   
     def MergeSortedArray1(nums1, nums2, m, n):
         for j in range(n):
@@ -38,7 +45,7 @@ class LeetCodeE:
     #Array2 = [2,5,6]
     #LeetCodeE().MergeSortedArray1(Array1 , Array2 , 5, 3)
     
-    
+    #Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
     @staticmethod  
     def removeElement1( nums, val):
         j=0
@@ -53,6 +60,9 @@ class LeetCodeE:
     #print(LeetCodeE().removeElement1(Array1, 2))
     #print(Array1)
     
+    #Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+    #Consider the number of unique elements of nums to be k
     @staticmethod  
     def removeDuplicatesSort1 (nums):
         j =1
@@ -64,6 +74,10 @@ class LeetCodeE:
                 j+=1
         return j
     
+    #Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+    #Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+    #Return k after placing the final result in the first k slots of nums.
+    #Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
     @staticmethod
     def removeDuplicatesSortTwice2 (nums):
         j=1
@@ -83,6 +97,9 @@ class LeetCodeE:
     #Array1 = [0,0,1,1,1,2,2,2,3,3,4]
     #print(LeetCodeE().removeDuplicatesSortTwice2(Array1))
     #print(Array1)
+    
+    #Given an array nums of size n, return the majority element.
+    #The majority element is the element that appears more than n/2 times. You may assume that the majority element always exists in the array.
     
     @staticmethod
     def MajorityElement (nums):
@@ -105,7 +122,7 @@ class LeetCodeE:
     #print(Array1)
     
 
-
+    #Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
     @staticmethod
     def RotateArray1(nums, k):
         k = k%len(nums)
@@ -127,6 +144,9 @@ class LeetCodeE:
     #LeetCodeE().RotateArray1(Array1,3)
     #print(Array1)
     
+    #You are given an array prices where prices[i] is the price of a given stock on the ith day.
+    #You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+    #Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
     @staticmethod
     def BestTimetoBuyandSellStock(prices):
         minValue = prices[0]
@@ -139,6 +159,9 @@ class LeetCodeE:
                 diff =max(diff,prices[i]-minValue)
         return diff
    
+    #You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+    #On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+    #Find and return the maximum profit you can achieve.
     @staticmethod
     def BestTimetoBuyandSellStock2(prices):
         diff = 0
@@ -180,6 +203,9 @@ class LeetCodeE:
                     
         return flag
 
+
+    #You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+    #Return true if you can reach the last index, or false otherwise.
     @staticmethod
     def JumpGame(nums):
         jump = 0
@@ -194,6 +220,12 @@ class LeetCodeE:
                     
         return True
   
+  
+    #You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+    #Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+    #0 <= j <= nums[i] and
+    #i + j < n
+    #Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
     
     
    
